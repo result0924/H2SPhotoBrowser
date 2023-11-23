ca420df7e257f3c814b271722b0966660a7c47ea
jlai <822363+result0924@users.noreply.github.com>
2023-11-23 17:56:26 +0800

    Add H2SPhotoBrowser framework

---
+++ H2SPhotoBrowser/H2SPhotoBrowser.h
@@ -0,0 +1,18 @@
+//
+//  H2SPhotoBrowser.h
+//  H2SPhotoBrowser
+//
+//  Created by Justin Lai on 2023/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for H2SPhotoBrowser.
+FOUNDATION_EXPORT double H2SPhotoBrowserVersionNumber;
+
+//! Project version string for H2SPhotoBrowser.
+FOUNDATION_EXPORT const unsigned char H2SPhotoBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <H2SPhotoBrowser/PublicHeader.h>
+
+
